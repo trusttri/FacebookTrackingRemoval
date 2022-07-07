0edd665ce7bb04c0b63919ee08509bda05ee9e14
trusttri <alwaysjane15@gmail.com>
2022-07-07 15:24:30 +0900

    fix button issue

--- src/app.js
+++ src/app.js
@@ -518,12 +518,14 @@ app.init().then(async () => {
                             // console.log(result.clickPersonal)
                             if (result.clickPersonal == "true") {
                                 // console.log("Redirect to personal data popup window");
-                                const button_class = "oajrlxb2 gs1a9yip g5ia77u1 mtkw9kbi tlpljxtp qensuy8j ppp5ayq2 goun2846 ccm00jje s44p3ltw mk2mc5f4 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv nhd2j8a9 mg4g778l pfnyh3mw p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x tgvbjcpo hpfvmrgz jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso l9j0dhe7 i1ao9s8h esuyzwwr f1sip0of du4w35lb btwxx1t3 abiwlrkh p8dawk7l lzcic4wl ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi a8c37x1j";
+                                // const button_class = "oajrlxb2 gs1a9yip g5ia77u1 mtkw9kbi tlpljxtp qensuy8j ppp5ayq2 goun2846 ccm00jje s44p3ltw mk2mc5f4 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv nhd2j8a9 mg4g778l pfnyh3mw p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x tgvbjcpo hpfvmrgz jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso l9j0dhe7 i1ao9s8h esuyzwwr f1sip0of du4w35lb btwxx1t3 abiwlrkh p8dawk7l lzcic4wl ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi a8c37x1j";
                                 setTimeout(() => {
-                                    // console.log(document.getElementsByClassName(button_class));
-                                    document.getElementsByClassName(button_class)[2].click();
+                                    // document.getElementsByClassName(button_class)[2].click();
+                                    var xpath = "//span[text()='Data about your activity from partners']";
+                                    var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                                    matchingElement.closest("[role='button']").click();
                                     chrome.storage.local.set({"clickPersonal": "false"}, function(){});
-                                }, 1500);
+                                }, 1000);
                             }
                         
                     });
