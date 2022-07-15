6c9db622d4465813e3afd64f5f114dd23c03bfc6
trusttri <alwaysjane15@gmail.com>
2022-07-15 07:02:24 -0400

    fix class name for opening popup

--- src/app.js
+++ src/app.js
@@ -519,9 +519,10 @@ app.init().then(async () => {
                     chrome.storage.local.get(["clickPersonal"], function(result){
                         if (result.clickPersonal == "true") {
                             // alert('Wait until a popup appears')
-                            const button_class = "oajrlxb2 gs1a9yip g5ia77u1 mtkw9kbi tlpljxtp qensuy8j ppp5ayq2 goun2846 ccm00jje s44p3ltw mk2mc5f4 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv nhd2j8a9 mg4g778l pfnyh3mw p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x tgvbjcpo hpfvmrgz jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso l9j0dhe7 i1ao9s8h esuyzwwr f1sip0of du4w35lb btwxx1t3 abiwlrkh p8dawk7l lzcic4wl ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi a8c37x1j";
+                            // const button_class = "oajrlxb2 gs1a9yip g5ia77u1 mtkw9kbi tlpljxtp qensuy8j ppp5ayq2 goun2846 ccm00jje s44p3ltw mk2mc5f4 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv nhd2j8a9 mg4g778l pfnyh3mw p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x tgvbjcpo hpfvmrgz jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso l9j0dhe7 i1ao9s8h esuyzwwr f1sip0of du4w35lb btwxx1t3 abiwlrkh p8dawk7l lzcic4wl ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi a8c37x1j";
+                            const button_class = "d2edcug0 hpfvmrgz qv66sw1b c1et5uql oi732d6d ik7dh3pa ht8s03o8 a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d9wwppkn iv3no6db a5q79mjw g1cxx5fr ekzkrbhg oo9gr5id hzawbc8m";
                             waitForBtn(button_class).then((elm) => { 
-                                elm[2].click();
+                                elm[0].click();
                                 chrome.storage.local.set({"clickPersonal": "false"}, function(){});
                             });
                         }
