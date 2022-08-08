15eeb736cd0c6ab8795b327531e1bd81c779d9de
Jane Im <alwaysjane15@gmail.com>
2022-08-08 00:01:27 -0400

    fixed id not being submitted

--- src/options.js
+++ src/options.js
@@ -24,9 +24,28 @@ app.init().then(() => {
     // Set version text
     // document.title += ` - v${browser.runtime.getManifest().version}`;
 
-    var form = document.getElementById("mainForm");
-    function handleForm(event) { event.preventDefault(); } 
-    form.addEventListener('id_submit', handleForm);
+    function submitButton() {
+        var prolific_ID = document.getElementById("sessionID").value;
+        chrome.storage.local.set({"prolific_ID": prolific_ID}, function(){});
+
+        if (prolific_ID.length === 0) {
+            document.getElementById("result").innerHTML = "Please type in your Proflic ID before starting";
+            document.getElementById("result").style.color = "white";
+            document.getElementById("resultBox").className = "callout"
+        } else {
+            document.getElementById("id_submit").disabled = true;
+            document.getElementById("result").textContent = "Signing up...";
+            document.getElementById("result").style.color = "white";
+            document.getElementById("resultBox").className = "callout"
+            browser.runtime.sendMessage("SIGN_UP");
+        }
+    }
+
+     document.getElementById('mainForm').addEventListener('keypress', function(e) {
+        if (e.key === 'Enter'){
+            submitButton();
+        }
+   });
 
 
     document.getElementById("sessionID").addEventListener("keyup", function(){
@@ -104,24 +123,10 @@ app.init().then(() => {
     })
 
     //submit prolific_id
-    document.getElementById("id_submit").addEventListener("click", function(){
-
-        var prolific_ID = document.getElementById("sessionID").value;
-        chrome.storage.local.set({"prolific_ID": prolific_ID}, function(){});
-
-        if (prolific_ID.length === 0) {
-            document.getElementById("result").innerHTML = "Please type in your Proflic ID before starting";
-            document.getElementById("result").style.color = "white";
-            document.getElementById("resultBox").className = "callout"
-        } else {
-            document.getElementById("id_submit").disabled = true;
-            document.getElementById("result").textContent = "Signing up...";
-            document.getElementById("result").style.color = "white";
-            document.getElementById("resultBox").className = "callout"
-            browser.runtime.sendMessage("SIGN_UP");
-        }
-        
-    })
+    document.getElementById("id_submit").addEventListener("click", function() {
+        console.log("submit prolific id");
+        submitButton()
+    });
 
     document.getElementById("reset").addEventListener("click", function(){
         chrome.storage.local.set({"log_history": []}, function(){console.log('reset')});
@@ -136,155 +141,4 @@ app.init().then(() => {
     })
 
 
-
-    // document.getElementById("legend").textContent += ` - v${browser.runtime.getManifest().version}`;
-
-    // function handleCheckbox() { app.options[this.id] = this.checked; }
-    // for (let checkbox of document.querySelectorAll("input[type=checkbox]")) {
-    //     checkbox.addEventListener("change", handleCheckbox);
-    // }
-
-    // function handleRadio() { app.options[this.name] = this.value; }
-    // for (let checkbox of document.querySelectorAll("input[type=radio]")) {
-    //     checkbox.addEventListener("change", handleRadio);
-    // }
-
-    // function handleText() {
-    //     this.value = this.value.trim();
-    //     if (!this.value) {
-    //         app.options.reset(this.id);
-    //     } else {
-    //         app.options[this.id] = this.value;
-    //     }
-    // }
-    // for (let text of document.querySelectorAll("input[type=text],textarea")) {
-    //     text.addEventListener("change", handleText);
-    // }
-
-
-    // modStyle.addEventListener("change", e => preview.style.cssText = this.value);
-
-    // function handleToggle() { document.getElementById(this.dataset.toggle).classList.toggle("hidden", !this.checked); }
-
-    // Avoid duplicated event listeners
-    // const dependFuncs = new Map();
-
-    // function init() {
-        // const opts = app.options;
-        // for (let key in opts) {
-        //     const value = opts[key];
-        //     const item = document.getElementById(key);
-        //     if (item) {
-        //         if (item.type === "checkbox") {
-        //             item.checked = value;
-        //         } else if (item.type === "text" || item.tagName === "TEXTAREA") {
-        //             if (!item.placeholder)
-        //                 item.placeholder = app.defaults[key];
-        //             item.value = value;
-        //         } else {
-        //             item.value = value;
-        //         }
-        //     } else {
-                // const radio = document.querySelector("input[name=" + key + "][value=" + value + "]");
-                // if (radio)
-                //     radio.checked = true;
-            // }
-        // }
-
-        // preview.style.cssText = modStyle.value;
-
-        // for (let elem of document.querySelectorAll("[data-depends]")) {
-        //     const source = document.getElementById(elem.dataset.depends);
-
-        //     if (!dependFuncs.has(elem))
-        //         dependFuncs.set(elem, () => elem.disabled = !source.checked);
-
-        //     source.addEventListener("change", dependFuncs.get(elem));
-        //     elem.disabled = !source.checked;
-        // }
-
-        // for (let checkbox of document.querySelectorAll("input[data-toggle]")) {
-        //     checkbox.addEventListener("change", handleToggle);
-        //     handleToggle.apply(checkbox);
-        // }
-    // }
-    // init();
-
-    // Per-option reset functionality
-    // function resetField(e) {
-    //     e.preventDefault();
-    //     const option = this.parentNode.querySelector("input[id],textarea[id],input[name]");
-    //     app.options.reset(option.id || option.name)
-    //         .then(() => this.parentNode.classList.add("resetDone"));
-    // };
-    // for (let reset of document.querySelectorAll("btn-reset")) {
-    //     const content = document.importNode(document.getElementById("btnReset").content, true);
-    //     content.firstElementChild.addEventListener("click", resetField);
-    //     reset.parentNode.replaceChild(content, reset);
-    // }
-    // document.body.addEventListener("animationend", e => e.target.classList.remove("resetDone"));
-
-
-    // Refresh button
-    // {
-        // const reloadTabs = () => browser.runtime.sendMessage("RELOAD");
-        // const btnRefresh = document.getElementById("btnRefresh");
-        // const btnText = btnRefresh.textContent;
-        // btnRefresh.title = browser.i18n.getMessage("optsRefreshHover", [RATE_LIMIT / 1000 / 60]);
-
-        // let timer;
-        // let disabled = false;
-
-        // function resetBtn() {
-        //     clearInterval(timer);
-        //     timer = null;
-        //     btnRefresh.textContent = btnText;
-        //     btnRefresh.disabled = disabled = false;
-        // }
-
-        // function btnRefreshTimer() {
-        //     resetBtn();
-        //     refreshRules({ check: true }).then(timeout => {
-        //         if (timeout <= 0)
-        //             return;
-
-        //         let remaining = Math.ceil(timeout / 1000);
-
-        //         btnRefresh.disabled = disabled = true;
-        //         btnRefresh.textContent = `${btnText} - ${remaining--} seconds`;
-
-        //         timer = setInterval(() => {
-        //             if (remaining <= 0) {
-        //                 resetBtn();
-        //             } else {
-        //                 btnRefresh.textContent = `${btnText} - ${remaining--} seconds`;
-        //             }
-        //         }, 1000);
-        //     });
-        // }
-
-        // btnRefreshTimer();
-        // btnRefresh.addEventListener("click", e => refreshRules({ force: e.ctrlKey }).then(reloadTabs).then(btnRefreshTimer));
-
-        // window.addEventListener("keydown", e => {
-        //     if (!e.repeat && e.key === "Control") {
-        //         btnRefresh.classList.add("ctrl");
-        //         btnRefresh.disabled = false;
-        //     }
-        // });
-        // window.addEventListener("keyup", e => {
-        //     if (e.key === "Control") {
-        //         btnRefresh.classList.remove("ctrl");
-        //         btnRefresh.disabled = disabled;
-        //     }
-        // });
-
-    // }
-
-
-    // Keep in sync with other options pages
-    // browser.storage.onChanged.addListener(() => app.init().then(init));
-
-    // Tell the background script a new options window was opened
-    // browser.runtime.sendMessage("OPTIONS");
 }, console.log);
