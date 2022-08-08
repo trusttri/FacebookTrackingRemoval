6abdadcbc94d5adf9a98e3ec0452d1f8c76ddd33
Jane Im <alwaysjane15@gmail.com>
2022-08-08 00:58:43 -0400

    log layout type

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@
 
 
 const BATCH_SIZE = 25;
-
+const QUERY_STRING_FOR_LAYOUT = ".thodolrn.ojvp67qx.taijpn5t.buofh1pr.j83agx80.aovydwv3.bqdfd6uv";
 
 // NOTE: Needs to run in IFrames as well because some options pages are loaded as IFrames
 
@@ -34,8 +34,12 @@ app.init().then(async () => {
 
     const _userRules = parseHideRules(app.options.userRules);
 
-    var topBar = document.querySelector(".thodolrn.ojvp67qx.taijpn5t.buofh1pr.j83agx80.aovydwv3.bqdfd6uv")
-    chrome.storage.local.set({"layout_type": "no_dropdown_icon"}, function(){console.log("no_dropdown_icon")});
+    var topBar = document.querySelector(QUERY_STRING_FOR_LAYOUT)
+    if(topBar){
+        chrome.storage.local.set({"layout_type": "no_dropdown_icon"}, function(){console.log("no_dropdown_icon")});
+    }else{
+        chrome.storage.local.set({"layout_type": "dropdown_icon"}, function(){console.log("dropdown_icon")});
+    }
                             
 
     function findClosestElementWithText(e) {
